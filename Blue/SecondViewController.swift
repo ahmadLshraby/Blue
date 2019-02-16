4acb17b46aa28d3a79491f7a911ccd6cde745215
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-17 01:54:49 +0200

    Initial Commit

---
+++ Blue/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Blue
+//
+//  Created by sHiKoOo on 2/17/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
