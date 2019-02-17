9fad15a13d9971b3748c48ad6ea2fd380db4e1b4
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-17 14:58:58 +0200

    MeVC
    Add new item in tabBar

---
+++ Blue/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  Blue
+//
+//  Created by sHiKoOo on 2/17/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOutBtn(_ sender: Any) {
+    }
+    
+
+
+}
