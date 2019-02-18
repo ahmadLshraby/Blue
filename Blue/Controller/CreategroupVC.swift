aa94557218a9b93894d3dded166cfbd962e67734
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-18 02:20:45 +0200

    Create new group UI

---
+++ Blue/Controller/CreategroupVC.swift
@@ -0,0 +1,36 @@
+//
+//  CreategroupVC.swift
+//  Blue
+//
+//  Created by sHiKoOo on 2/18/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class CreategroupVC: UIViewController {
+
+    @IBOutlet weak var titleTxt: InsetTextField!
+    @IBOutlet weak var descTxt: InsetTextField!
+    @IBOutlet weak var emailTxt: InsetTextField!
+    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var addPeopleLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func doneBtn(_ sender: UIButton) {
+    }
+    
+    
+ 
+
+}
